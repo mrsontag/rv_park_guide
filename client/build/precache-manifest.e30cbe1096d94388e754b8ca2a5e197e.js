aafb5a2704417d33ee828087d442df66eadc4e2f
Matt Sontag <mrsontag@gmail.com>
2020-10-17 22:16:25 -0400

    committing build

---
+++ client/build/precache-manifest.e30cbe1096d94388e754b8ca2a5e197e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942205994c06583aac55226bdd58827c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f229e993f594d0b09a2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1db2f70b0377e9cf2565",
+    "url": "/static/js/2.5fcf0f5a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5fcf0f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f229e993f594d0b09a2",
+    "url": "/static/js/main.8651247d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
